b7c5d7eca565e4d0f65603e93f4154eda5e932a9
FrankJKB <balestrat_f@live.ca>
2022-01-26 16:05:48 -0500

    Update functions file

--- plotting_functions_f.py
+++ plotting_functions_f.py
@@ -8,25 +8,25 @@ Created on Wed Jan 19 08:52:02 2022
 #IMPORT LIBRARIES/PACKAGES REQUIRED
 import pandas as pd
 import plotly.graph_objects as go
-import dash
-import dash_html_components as html
-import dash_core_components as dcc
-#!pip install dash-bootstrap-components
-import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State
 import requests
-import urllib
-import urllib.request
-import time 
 import re 
-import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
-import seaborn as sns
-import numpy as np
-import plotly.express as px
+#import plotly.express as px
+#import dash
+#import dash_html_components as html
+#import dash_core_components as dcc
+#import dash_bootstrap_components as dbc
+#from dash.dependencies import Input, Output, State
+#import urllib
+#import urllib.request
+#import time 
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+#import numpy as np
+
+#*********************************************FUNCTION DEFINITIONS *******************************
 
 def web_scraping():
-    
     #WEB SCRAPING
     #CONNECT TO URL FOR WEB SCRAPING
     url = 'https://insete.gr/perifereies/' 
@@ -36,7 +36,7 @@ def web_scraping():
     #FIND ALL LINK OBJECTS
     link_objs = soup.find_all('a',href=re.compile('/wp-content/uploads/'))
     
-    #WEB SCRAPING EXCEL FILE FROM WEBSITE
+    #WEB SCRAPING EXCEL FILES FROM WEBSITE
     file_names = []
     region_names = []
     for i in range(len(link_objs)):
@@ -45,19 +45,21 @@ def web_scraping():
         end_index = re.search(r"\"",str_link[start_index.span()[0]:len(str_link)])
         end_url = str_link[start_index.span()[0]:start_index.span()[0]+end_index.span()[0]]
         sub_url = 'https://insete.gr/'+end_url
-
+        
+        #FIND ALL STRINGS THAT CONTAIN AN EXCEL FILE EXTENSION
         if ".xlsx" in sub_url:
             file_names.append(sub_url)
             region_start_index = re.search(r"uploads",sub_url)
             region_end_index = re.search(r".xlsx",sub_url[region_start_index.span()[0]:len(sub_url)])
             region_end_url = sub_url[region_start_index.span()[0]+22:region_start_index.span()[0]+region_end_index.span()[0]-2]
+            #CLEAN STRINGS
             region_end_url = re.sub("_", " ", region_end_url)
             region_end_url = re.sub("  ", " ", region_end_url)
             region_names.append(region_end_url)
-
+    
+    #RETURN REGION NAMES AND FILE NAMES
     return region_names, file_names
 
-#GRAPH 1 ------------------------------------------------------------ *NOT USED
 def employment_data(file_names):
     
     #EMPLOYMENT DATA ANALYSIS
@@ -65,72 +67,71 @@ def employment_data(file_names):
     pd.set_option('display.float_format', lambda x: '%.2f' % x)
     tab_name = "Employment"
     list_of_df = list()
-
+    
+    #MANUALLY INPUT NAMES IN ENGLISH (TRANSLATED FROM GREEK)
     names = ["Year", "Accommodation and Catering Services Activities", "Other Industries", 
              "Total Employment", "Country Total", "% Services in Relation to the Whole Region", "% Other Branches in Relation to the Whole Region"]
-
+    
+    #OBTAIN ALL EMPLOYMENT DATA FROM EACH EXCEL FILE
     for i in range(len(file_names)):
         x = requests.get(file_names[i]).content
         file = pd.ExcelFile(x)
+        #GET DATA FROM EMPLOYMENT TAB
         df = file.parse(tab_name, header = 3, skipfooter = 14, index_col = 0)
-
+        
+        #CLEAN DATA
         df.reset_index(inplace=True, drop=True)
         df = df.T
         df.reset_index(inplace=True)
         df.columns = names
         
+        #MULTIPLY CERTAIN ROWS BY 1000 (TABLE INDICATED THAT DATA WAS IN THOUSANDS)
         for i in range(1, 5):
             df.iloc[:,i] = df.iloc[:,i]*1000
         
+        #MULTIPLY CERTAIN ROWS BY 100 (TABLE INDICATED THAT DATA WAS IN HUNDREDS)
         for i in range(5, 7):
             df.iloc[:,i] = df.iloc[:,i]*100
         
         list_of_df.append(df)
     
+    #RETURN A LIST OF DATAFRAMES WITH EACH DATAFRAME BEING THE DATA FOUND IN THE EMPLOYMENT FILE
+    #IN DIFFERENT EXCEL FILES (REGIONS)
     return list_of_df
 
-def graph_employment_per_year(list_of_df, year):
-
-    #TOTAL COUNTRY EMPLOYMENT GRAPH
-    #*************TODO : labels X and Y
-    i = year[0] - 2010
-    j = year[1] - 2010 + 1
-    fig = px.line(list_of_df[0], x=list_of_df[0]["Year"][i:j], y=list_of_df[0]["Country Total"][i:j], width=800, height=600)
-    
-    return fig
-
-#GRAPH 2 ------------------------------------------------------------
 def employment_per_region(list_of_df, region_names):
 
     #EMPLOYMENT PER REGION DATA
-    tot = pd.DataFrame(list_of_df[0]["Year"])
+    employ_data = pd.DataFrame(list_of_df[0]["Year"])
 
     for i in range(0, 13):
-        tot = tot.join(pd.DataFrame(list_of_df[i].iloc[:,3]))
-        tot = tot.rename(columns={tot.columns[i+1]: region_names[i]})
+        employ_data = employ_data.join(pd.DataFrame(list_of_df[i].iloc[:,3]))
+        employ_data = employ_data.rename(columns={employ_data.columns[i+1]: region_names[i]})
         
-    return tot
+    return employ_data
 
-def employment_data_graph(tot, year, region_names):
+def employment_data_graph(employ_data, year, region_names):
 
     #EMPLOYMENT PER REGION GRAPH   
-    fig2 = go.Figure()
+    fig1 = go.Figure()
+    #DEFINE RANGE OF YEARS TO BE PLOTTED BASED ON INPUT YEARS
     years = [*range(year[0], year[1] + 1, 1)]
-    fig2.add_trace(go.Scatter(name = region_names[0], x = years, y = tot.iloc[:,0], fill='tozeroy', 
+    fig1.add_trace(go.Scatter(name = region_names[0], x = years, y = employ_data.iloc[:,0], fill='tozeroy', 
                               mode = "none", stackgroup='one'))
     for i in range(1, 13):
-        fig2.add_trace(go.Scatter(name = region_names[i], x = years, y = tot.iloc[:,i], fill='tonexty', 
+        fig1.add_trace(go.Scatter(name = region_names[i], x = years, y = employ_data.iloc[:,i], fill='tonexty', 
                                   mode = "none", stackgroup='one'))           
-    fig2.update_layout(width = 800, height = 600, xaxis_title='Year', yaxis_title='Employment')
-    return fig2
+    fig1.update_layout(width = 800, height = 600, xaxis_title='Year', yaxis_title='Employment')
+    #RETURN GRAPH
+    return fig1
 
 #GRAPH 3 ------------------------------------------------------------
 def arrivals_per_country():
 
     #ARRIVALS PER COUNTRY ANALYSIS
     #WEBSCRAPING EXCEL FILE FOR ARRIVALS PER COUNTRY
-    url = 'https://insete.gr/statistika-eiserxomenou-tourismou/' 
-    response = requests.get(url) 
+    #url = 'https://insete.gr/statistika-eiserxomenou-tourismou/' 
+    #response = requests.get(url) 
     file_name = 'https://insete.gr/wp-content/uploads/2020/06/Key_figures_of_incoming_TourismGR-1.xlsx'
     x = requests.get(file_name).content
     file = pd.ExcelFile(x)
@@ -168,8 +169,8 @@ def region_population_vs_tourism(file_names, region_names):
 
     #REGION POPULATION VS INCOMING TOURISM (INTERNATIONAL + DOMESTIC) ANALYSIS
     #2020 TOTAL INTERNATIONAL/DOMESTIC AIR ARRIVALS PER REGION
-    url = 'https://insete.gr/perifereies/' 
-    response = requests.get(url) 
+    #url = 'https://insete.gr/perifereies/' 
+    #response = requests.get(url) 
     tab_names = ["Intern-Domestic Air Arrivals"]*13 
     tab_names[1] = "Internat-domestic air arrivals"
     tab_names[4] = "Domestic Air Arrivals"
@@ -203,29 +204,10 @@ def region_population_vs_tourism(file_names, region_names):
     
     return data
 
-def arrivals_per_region_graph(data):
 
-    #GRAPH ARRIVALS DATA
-    data.sort_values(by = "Total Domestic Air Arrivals", axis = 0, ascending = True, inplace = True)
-
-    fig3 = go.Figure()                                                           
-    fig3.add_trace(go.Bar(x = data["Total International Air Arrivals"], y = data["Region"], name = "International Arrivals", 
-                          marker=dict(line=dict(width=0.1)), orientation = 'h'))
-    fig3.add_trace(go.Bar(x = data["Total Domestic Air Arrivals"], y = data["Region"], name = "Domestic Arrivals",
-                          marker=dict(line=dict(width=0.1)), orientation = 'h'))
-    fig3.update_traces(width=0.65)
-    fig3.update_layout(xaxis=dict(title_text="Total Arrivals", titlefont=dict(size=15), zeroline=False),
-                       yaxis=dict(titlefont=dict(size=15)), width=800, height=600,
-                       title={'text': "Total Air Arrivals in 2020 per Region", 'y':0.92, 'x':0.5, 
-                              'xanchor': 'center','yanchor': 'top'}, barmode='stack', bargap=0.0)
-    fig3.show()
-    
-    return fig3
-
-#GRAPH 5 ------------------------------------------------------------
+#GRAPH 3 ------------------------------------------------------------
 def import_population_data(data):
     #IMPORT EXCEL FILE WITH POPULATION DATA PER REGION
-    #TODO***WEBSCRAPE POPULATION DATA INSTEAD OF IMPORT EXCEL FILE***
     pop_data = pd.read_excel(r'Population_data_per_region.xlsx')
     data2 = pd.merge(data, pop_data, on='Region')
     
